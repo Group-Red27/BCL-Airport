33f68f5e357c33963a22e748345fcb4263cf3df8
MrSamerM <2106040@brunel.ac.uk>
2022-02-01 15:19:12 +0000

    I added setters + getters

--- src/SeatingPlan.java
+++ src/SeatingPlan.java
@@ -1,9 +1,46 @@
 
 public class SeatingPlan {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private int numOfTickets;
+		private String planeType;
+		private String[] seatNumber;
+		private boolean[] seatAvailability;
+		private String[][] seatClass;
+		
+		
+		public int getNumOfTickets() {
+			return numOfTickets;
+		}
+		public void setNumOfTickets(int numOfTickets) {
+			this.numOfTickets = numOfTickets;
+		}
+		public String getPlaneType() {
+			return planeType;
+		}
+		public void setPlaneType(String planeType) {
+			this.planeType = planeType;
+		}
+		public String[] getSeatNumber() {
+			return seatNumber;
+		}
+		public void setSeatNumber(String[] seatNumber) {
+			this.seatNumber = seatNumber;
+		}
+		public boolean[] getSeatAvailability() {
+			return seatAvailability;
+		}
+		public void setSeatAvailability(boolean[] seatAvailability) {
+			this.seatAvailability = seatAvailability;
+		}
+		public String[][] getSeatClass() {
+			return seatClass;
+		}
+		public void setSeatClass(String[][] seatClass) {
+			this.seatClass = seatClass;
+		}
+		
 
 	}
 
-}
+	
+
