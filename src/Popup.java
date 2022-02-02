cb96a8b8855131ac9fe74794f20ab7b207c827cd
MrSamerM <2106040@brunel.ac.uk>
2022-02-02 14:04:20 +0000

    added messages to pop up class

--- src/Popup.java
+++ src/Popup.java
@@ -3,12 +3,12 @@ import javax.swing.JOptionPane;
 public class Popup extends JOptionPane
 {
 	public static void showErrorMessage(String errorString) {
-		JOptionPane.showMessageDialog( null, errorString , "Error", JOptionPane.ERROR_MESSAGE );
-	}
+		JOptionPane.showMessageDialog( null, errorString , "Error", JOptionPane.ERROR_MESSAGE ); 
+	}  //Copy paste for an error message for your own component. Just change the message.
 	
 	
 	public static void showSuccessMessage(String errorString) {
 		JOptionPane.showMessageDialog( null, errorString , "Success", JOptionPane.INFORMATION_MESSAGE );
-	}
+	}	//Copy paste for an Success message for your own component. Just change the message.
 	
 }
