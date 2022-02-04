f1dc25f5e332f6638533565da7eb16b8678a1edf
MrSamerM <2106040@brunel.ac.uk>
2022-02-04 19:51:27 +0000

    updated UI

---
+++ src/AirbusA318_and_Boeing747_SeatingPlan.java
@@ -0,0 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AirbusA318_and_Boeing747_SeatingPlan extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AirbusA318_and_Boeing747_SeatingPlan frame = new AirbusA318_and_Boeing747_SeatingPlan();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AirbusA318_and_Boeing747_SeatingPlan() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
