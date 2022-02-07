9cc103f8cb645016c777a529c481d7281c422a2d
dareen <2139667@brunel.ac.uk>
2022-02-07 18:11:25 +0000

    flightclass

--- src/Flightclass.java
+++ src/Flightclass.java
@@ -1,4 +1,31 @@
-
 public class Flightclass {
 
+	int dateofflight;
+	int departuretime;
+	int arrivaltime;
+	int flightduration;
+	double distance;
+	int delay;
+	int departdate;
+	int returndate;
+	String departureairport;
+	String departurecity;
+	String arrivalairport;
+	String arrivalcity;
+	String flightnumber;
+	String airline;
+	String[] returnfligth;
+	String[] departureflight;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
