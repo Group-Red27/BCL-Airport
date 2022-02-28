aa890ef6422df3a8256a120e13d9d6599eae337e
Jazib Jawed (Student) <2018118@brunel.ac.uk>
2022-02-28 16:46:10 +0000

    added passenger UI

--- src/Passengers.java
+++ src/Passengers.java
@@ -1,4 +1,23 @@
+import javax.swing.JOptionPane;
+
 
 public class Passengers {
+	public static void main(String[] args) {
+		String fullName = JOptionPane.showInputDialog("Enter your First Name");
+		int spacePosition = fullName.indexOf("");
+		
+		String firstName = fullName.substring(spacePosition);
+		String surName = fullName.substring(spacePosition+1);
+		
+		JOptionPane.showMessageDialog(null, "your First Name is:" + firstName + " Surname is;" + surName);
+		
+		
+				
+		
+	}
 
 }
+
+	
+
+
