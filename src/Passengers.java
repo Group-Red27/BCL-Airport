1151065aef99c58c01dd6e591e95db3a5dd207de
Jazib Jawed (Student) <2018118@brunel.ac.uk>
2022-02-28 17:41:23 +0000

    connected first name with UI

--- src/Passengers.java
+++ src/Passengers.java
@@ -1,21 +1,19 @@
 import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
 
 
 public class Passengers {
-	public static void main(String[] args) {
-		String fullName = JOptionPane.showInputDialog("Enter your First Name");
-		int spacePosition = fullName.indexOf("");
-		
-		String firstName = fullName.substring(spacePosition);
-		String surName = fullName.substring(spacePosition+1);
-		
-		JOptionPane.showMessageDialog(null, "your First Name is:" + firstName + " Surname is;" + surName);
-		
-		
-				
+	
+	
+		public boolean validateName(JTextPane entry) {
+		entry.getText();
+		return false;
 		
 	}
-
+	
+		// validate passenger name(passengerENtry)
+	//passengerentry.gettext
+	
 }
 
 	
